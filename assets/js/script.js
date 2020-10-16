16890ea45531bf27d8e370dd52c616254a0b04e9
mihaelasandrea <mihaelasandrea@yahoo.com>
2020-10-16 08:38:09 +0000

    Add script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+class audioController {
+    
+}
+
+function ready(){
+    let overlays = Array.from(document.getElementsByClassName("overlay-text"));
+    let cards = Array.from(document.getElementsByClassName("card"));
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener("click", () => {
+            overlay.classList.remove("visible");
+            //game.startGame();
+        });
+    });
+
+    cards.forEach(card => {
+        card.addEventListener("click", () => {
+            //game.flipCard();
+        });
+    });
+};
+
+if(document.readyState === loading) {
+    document.addEventListener("DOMContentLoaded", ready());
+} else {
+    ready();
+}
